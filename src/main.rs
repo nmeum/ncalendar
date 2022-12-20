deef61831150e04bc3dee8873e2db91e5288c192
Sören Tempel <soeren+git@soeren-tempel.net>
2022-12-20 06:37:52 +0100

    main: use ~/.ncalendar/calendar as the default calendar file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 extern crate ncalendar;
 
 use std::env;
+use std::path::{self, Path};
+
+fn calendar_file() -> Result<path::PathBuf, env::VarError> {
+    let home = env::var("HOME")?;
+    let path = Path::new(&home);
+
+    Ok(path
+        .join(".ncalendar")
+        .join("calendar"))
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Error: Supply exactly one argument");
-    }
+    let fp = calendar_file().unwrap();
 
-    let entries = ncalendar::parse_file(&args[1]).unwrap();
+    let entries = ncalendar::parse_file(fp.as_path()).unwrap();
     for entry in entries.iter() {
         println!("Entry: {:?} - {:?}", entry.day, entry.desc);
     }
