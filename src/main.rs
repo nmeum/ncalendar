792510a5dc51e4c14f330ae85c9eb08110ca864d
Sören Tempel <soeren+git@soeren-tempel.net>
2022-12-20 09:54:59 +0100

    Add command-line parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 extern crate ncalendar;
+extern crate structopt;
 
 use std::env;
 use std::path::{self, Path};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Opt {
+    /// Use the given file as the default calendar file.
+    #[structopt(short = "f", parse(from_os_str))]
+    file: Option<path::PathBuf>,
+
+    /// Amount of next days to consider.
+    #[structopt(short = "a", default_value = "0")]
+    num: u32,
+}
 
 fn calendar_file() -> Result<path::PathBuf, env::VarError> {
     let home = env::var("HOME")?;
@@ -11,6 +25,7 @@ fn calendar_file() -> Result<path::PathBuf, env::VarError> {
 }
 
 fn main() {
+    let opt = Opt::from_args();
     let fp = calendar_file().unwrap();
 
     let entries = ncalendar::parse_file(fp.as_path()).unwrap();
