d054bce91829c178993d97768085141d4f8027ac
Sören Tempel <soeren+git@soeren-tempel.net>
2022-12-21 03:31:13 +0100

    Only select Entries matched by the -a option

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate ncalendar;
 extern crate structopt;
 
+use ncalendar::{Entry, Reminder};
 use std::env;
 use std::path::{self, Path};
 use structopt::StructOpt;
@@ -13,15 +14,57 @@ struct Opt {
     file: Option<path::PathBuf>,
 
     /// Amount of next days to consider.
-    #[structopt(short = "a", default_value = "0")]
+    #[structopt(short = "a", default_value = "1")]
     num: u32,
 }
+
+#[derive(PartialEq)]
+enum Direction {
+    Future,
+    Past,
+}
+
+/// Represents a time span between two dates.
+struct TimeSpan {
+    start: time::Date,
+    end: time::Date,
+}
+
+impl TimeSpan {
+    pub fn new(day: time::Date, dur: time::Duration, dir: Direction) -> Option<Self> {
+        let (start, end) = if dir == Direction::Future {
+            (day, day.checked_add(dur)?)
+        } else {
+            (day.checked_sub(dur)?, day)
+        };
+
+        Some(TimeSpan { start, end })
+    }
+
+    pub fn contains(&self, d: time::Date) -> bool {
+        d >= self.start && d <= self.end
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
 
 fn calendar_file() -> Result<path::PathBuf, env::VarError> {
     let home = env::var("HOME")?;
     let path = Path::new(&home);
 
     Ok(path.join(".ncalendar").join("calendar"))
+}
+
+/// Check if entry matches for the given date (assume today for now).
+fn matches(t: &TimeSpan, e: &Entry) -> bool {
+    match e.day {
+        Reminder::Weekday(_wday) => {
+            // TODO: Requires reasoning about WeekDay + 1
+            // but time doesn't implement Ord or PartialOrd.
+            false
+        }
+        Reminder::Date(date) => t.contains(date),
+    }
 }
 
 fn main() {
@@ -32,8 +75,15 @@ fn main() {
         calendar_file().unwrap()
     };
 
+    let today = time::OffsetDateTime::now_local().unwrap().date();
+    let duration = time::Duration::days(opt.num.into());
+    let span = TimeSpan::new(today, duration, Direction::Future).unwrap();
+
     let entries = ncalendar::parse_file(fp.as_path()).unwrap();
-    for entry in entries.iter() {
+    let filtered = entries.iter().filter(|entry| matches(&span, entry));
+
+    // TODO: Filter entries using the matches method below and then print them.
+    for entry in filtered {
         println!("Entry: {:?} - {:?}", entry.day, entry.desc);
     }
 }
